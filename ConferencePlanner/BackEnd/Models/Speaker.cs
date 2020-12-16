2287729e822ea36eabcef210e106581eee80b9b7
Ivan Kalinić <ik002312821@fer.hr>
2020-12-16 11:55:38 +0100

    Initial commit

---
+++ ConferencePlanner/BackEnd/Models/Speaker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace BackEnd.Models
+{
+    public class Speaker
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public int Name { get; set; }
+
+        [StringLength(1000)]
+        public string Bio { get; set; }
+
+        [StringLength(200)]
+        public virtual string WebSite { get; set; }
+
+    }
+}
