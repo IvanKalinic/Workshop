dded48e3190555c14516d2844e9cfc6cef666714
Ivan Kalinić <ik002312821@fer.hr>
2020-12-18 21:15:00 +0100

    Modify speakers controller,added new API controllers and DTOs and added conference upload support

--- ConferencePlanner/BackEnd/Data/Speaker.cs
+++ ConferencePlanner/BackEnd/Data/Speaker.cs
@@ -9,6 +9,6 @@ namespace BackEnd.Data
 {
     public class Speaker : ConferenceDTO.Speaker
     {
-
+        public virtual ICollection<SessionSpeaker> SessionSpeakers { get; set; } = new List<SessionSpeaker>();
     }
 }
