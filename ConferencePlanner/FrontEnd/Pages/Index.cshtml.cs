5c1efb8d4f1eb59109363d133d90fd51781a4eb9
Ivan Kalinić <ik002312821@fer.hr>
2020-12-19 16:50:02 +0100

    Added FrontEnd project and Services inside of it

---
+++ ConferencePlanner/FrontEnd/Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConferenceDTO;
+using FrontEnd.Services;
+
+namespace FrontEnd.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        protected readonly IApiClient _apiClient;
+
+        public IndexModel(IApiClient apiClient,ILogger<IndexModel> logger)
+        {
+            _apiClient = apiClient;
+            _logger = logger;
+        }
+
+        public IEnumerable<IGrouping<DateTimeOffset?, SessionResponse>> Sessions { get; set; }
+
+        public IEnumerable<(int Offset, DayOfWeek? DayofWeek)> DayOffsets { get; set; }
+
+        public int CurrentDayOffset { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
