9147e2941326fc0598e7a2e2625948bed5d8eaef
Ivan Kalinić <ik002312821@fer.hr>
2020-12-20 10:47:47 +0100

    Changed ServiceUrl

--- ConferencePlanner/FrontEnd/Pages/Index.cshtml.cs
+++ ConferencePlanner/FrontEnd/Pages/Index.cshtml.cs
@@ -28,8 +28,26 @@ namespace FrontEnd.Pages
 
         public int CurrentDayOffset { get; set; }
 
-        public void OnGet()
+        public async Task OnGet(int day = 0)
         {
+            CurrentDayOffset = day;
+
+            var sessions = await _apiClient.GetSessionsAsync();
+
+            var startDate = sessions.Min(s => s.StartTime?.Date);
+
+            var offset = 0;
+            DayOffsets = sessions.Select(s => s.StartTime?.Date)
+                .Distinct()
+                .OrderBy(d => d)
+                .Select(day => (offset++, day?.DayOfWeek));
+
+            var filterDate = startDate?.AddDays(day);
+
+            Sessions = sessions.Where(s => s.StartTime?.Date == filterDate)
+                .OrderBy(s => s.TrackId)
+                .GroupBy(s => s.StartTime)
+                .OrderBy(g => g.Key);
 
         }
     }
